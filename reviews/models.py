fff328077592d234b67b722a7816f28c8392d5eb
soongon <soongon@hucloud.co.kr>
2021-11-16 16:32:59 +0900

    initial commit

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=50)
+    website = models.URLField()
+    email = models.EmailField()
