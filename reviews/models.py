c6463b2d9a6e72a260a4f349896f7d8c76609817
soongon <soongon@hucloud.co.kr>
2021-11-16 17:20:29 +0900

    initial commit

--- reviews/models.py
+++ reviews/models.py
@@ -5,3 +5,15 @@ class Publisher(models.Model):
     name = models.CharField(max_length=50)
     website = models.URLField()
     email = models.EmailField()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=70)
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=20)
+
+
+class Contributor(models.Model):
+    first_names = models.CharField(max_length=50)
+    last_names = models.CharField(max_length=50)
+    email = models.EmailField()
