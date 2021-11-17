8bfe415440790a984a9f62408420687a26c289b1
soongon <soongon@hucloud.co.kr>
2021-11-17 14:09:23 +0900

    initial commit

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import Book, Publisher, Contributor, Review, BookContributor
+
+admin.site.register(Book)
+admin.site.register(Publisher)
+admin.site.register(Contributor)
+admin.site.register(Review)
+admin.site.register(BookContributor)
