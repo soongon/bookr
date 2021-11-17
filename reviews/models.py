8c8415b5fdd9aa1e2dc939de39059463a3ec956f
soongon <soongon@hucloud.co.kr>
2021-11-17 11:28:20 +0900

    initial commit

--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import auth
 
 
 class Publisher(models.Model):
@@ -11,9 +12,32 @@ class Book(models.Model):
     title = models.CharField(max_length=70)
     publication_date = models.DateField()
     isbn = models.CharField(max_length=20)
+    publisher = models.ForeignKey(
+        Publisher, on_delete=models.CASCADE)
+    contributors = models.ManyToManyField('Contributor', through='BookContributor')
 
 
 class Contributor(models.Model):
     first_names = models.CharField(max_length=50)
     last_names = models.CharField(max_length=50)
     email = models.EmailField()
+
+
+class BookContributor(models.Model):
+    class ContributorRole(models.TextChoices):
+        AUTHOR = 'AUTHOR', 'Author'
+        CO_AUTHOR = 'CO_AUTHOR', 'Co-Author'
+        EDITOR = 'EDITOR', 'Editor'
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
+    role = models.CharField(choices=ContributorRole.choices, max_length=20)
+
+
+class Review(models.Model):
+    content = models.TextField()
+    rating = models.IntegerField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_edited = models.DateTimeField(null=True)
+    creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
